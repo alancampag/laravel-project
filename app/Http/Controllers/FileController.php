bc93aedafed5d5afc34f5b7cd30b501139b72880
alan <alancampagnaro@gmail.com>
2020-02-01 16:12:47 -0300

    file upload fixed

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -8,17 +8,17 @@ class FileController extends Controller
 {
     public function saveFile(Request $request) {
         $validated = $this->validateCSV();
-        // $file = $validated->file('csvFile');
-        // $path = $file->store('uploads');
-        // return $path;
-        dd($request);
+        $file = $validated['csvFile'];   
+        $path = $file->store('uploads');
+        return $path;
     }
 
     public function validateCSV() { 
         return request()->validate([
-            'csvFile' => 'required|mimes:csv,xlsx,xls'
+            'csvFile' => 'required|mimes:csv,txt'
         ]);
     }
 }
+
 
 
